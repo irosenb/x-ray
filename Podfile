198ece5cc19b53c9bd33221d2ea014c3c58399ad
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-21 22:20:45 -0700

    first commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+source 'https://github.com/CocoaPods/Specs.git'
+
+target 'WifiPassword' do
+
+end
+
+target 'WifiPasswordTests' do
+
+end
+
---
+++ WifiPassword/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  WifiPassword
+//
+//  Created by Isaac Rosenberg on 10/21/14.
+//  Copyright (c) 2014 irosenb. All rights reserved.
+//
+
+import Cocoa
+import CoreWLAN/CoreWLAN.h
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
