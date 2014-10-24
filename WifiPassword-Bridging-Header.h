5341c08fcaafa5038c2ae82ca448f1e6d73ab11d
Isaac Rosenberg <irosenb7@gmail.com>
2014-10-24 16:15:18 -0700

    gitignore build

---
+++ WifiPassword-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  WifiPassword-Bridging-Header.h
+//  WifiPassword
+//
+//  Created by Isaac Rosenberg on 10/22/14.
+//  Copyright (c) 2014 irosenb. All rights reserved.
+//
+
+#ifndef WifiPassword_WifiPassword_Bridging_Header_h
+#define WifiPassword_WifiPassword_Bridging_Header_h
+
+
+#endif
---
+++ WifiPassword/MasterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MasterViewController.swift
+//  WifiPassword
+//
+//  Created by Isaac Rosenberg on 10/22/14.
+//  Copyright (c) 2014 irosenb. All rights reserved.
+//
+
+import Cocoa
+
+class MasterViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
