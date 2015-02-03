10a9242e85f8128a8310e8e42a33853ae471215b
Isaac Rosenberg <irosenb7@gmail.com>
2015-02-03 12:32:01 -0800

    Reorganized

--- WifiPassword/AppDelegate.swift
+++ WifiPassword/AppDelegate.swift
@@ -23,20 +23,26 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var menu: NSMenu = NSMenu()
     var menuItem : NSMenuItem = NSMenuItem()
     var wifiName : String!
+    var controller = MasterViewController(windowNibName: "MasterViewController")
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         window.styleMask = NSBorderlessWindowMask
+        controller.showWindow(nil)
     }
     
     override func awakeFromNib() {
         statusBarItem = statusBar.statusItemWithLength(-1)
         statusBarItem.menu = menu
         statusBarItem.title = "password"
-        
+
         menuItem.title = "Show"
-        menuItem.action = Selector("setWindowVisible:")
+        menuItem.action = Selector("openWifiWindow:")
         menuItem.keyEquivalent = "p"
         menu.addItem(menuItem)
+    }
+    
+    func openWifiWindow(sender: AnyObject) {
+        controller.showPassword(self)
     }
     
     func setWindowVisible(sender: AnyObject) {
--- WifiPassword/MasterViewController.swift
+++ WifiPassword/MasterViewController.swift
@@ -10,45 +10,52 @@ import Cocoa
 import CoreWLAN
 
 class MasterViewController: NSWindowController {
-    
-//    @IBOutlet weak var window: NSWindow!
+
     @IBOutlet weak var password: NSTextField!
     
+    @IBOutlet var wifiWindow: NSWindow!
     @IBOutlet weak var wifiField: NSTextField!
     
     var statusBar = NSStatusBar.systemStatusBar()
     var statusBarItem: NSStatusItem = NSStatusItem()
     var menuItem : NSMenuItem = NSMenuItem()
     var wifiMenu = NSMenu()
-    var wifi: Wifi!
+    var wifi: Wifi?
 
     override func windowDidLoad() {
         super.windowDidLoad()
         println("Hello world")
-        statusBarItem = statusBar.statusItemWithLength(-1)
-        statusBarItem.menu = menu
-        statusBarItem.title = "password"
-        
-        menuItem.title = "Show"
-        menuItem.action = Selector("showPassword:")
-        menuItem.keyEquivalent = "p"
-        wifiMenu.addItem(menuItem)
+        wifiWindow.styleMask = NSBorderlessWindowMask
+//        showPassword(self)
+//        statusBarItem = statusBar.statusItemWithLength(-1)
+//        statusBarItem.menu = menu
+//        statusBarItem.title = "password"
+//        
+//        menuItem.title = "Show"
+//        menuItem.action = Selector("showPassword:")
+//        menuItem.keyEquivalent = "p"
+//        wifiMenu.addItem(menuItem)
     }
     
     func showPassword(sender: AnyObject) {
-        if (wifi.network != Wifi().network) {
+        if (wifi?.network != Wifi().network) {
             getPassword()
         }
-        window!.center()
-        self.window!.orderFrontRegardless()
-        self.window!.orderedIndex = 0
+        wifiWindow.center()
+        wifiWindow.makeFirstResponder(self)
+        wifiWindow.makeKeyWindow()
+//        wifiWindow.makeKeyAndOrderFront(wifiWindow)
+        println(wifiWindow.keyWindow)
+        self.wifiWindow.orderFrontRegardless()
+//        self.wifiWindow.orderedIndex = 0
+
     }
     
     func getPassword() {
         wifi = Wifi()
         
-        wifiField.stringValue = wifi.network
-        password.stringValue = wifi.password
+        wifiField.stringValue = wifi!.network
+        password.stringValue = wifi!.password
     }
     
 }
